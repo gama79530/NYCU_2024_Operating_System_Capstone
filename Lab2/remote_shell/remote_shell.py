b73f6fcbf22b69de58698fa8f05c490467f33ad0
gama79530 <gama79530@gmail.com>
2024-06-21 11:49:00 +0800

    Modify: adjust the function of read from serial

--- Lab2/remote_shell/remote_shell.py
+++ Lab2/remote_shell/remote_shell.py
@@ -4,85 +4,108 @@ import serial
 import re
 import select
 import termios
+import time
 
 
 kernel_path = './Lab2/kernel/kernel8.img'
-port = 'ttyUSB0'
+port = '/dev/ttyUSB0'
 baudrate = 115200
-communicator = serial.Serial(baudrate=baudrate)
-line_width = 128
+communicator = serial.Serial(port=None, baudrate=baudrate)
+buffer = ''
 
 def configure_shell():
     global kernel_path
     global port
+    global communicator
+    global buffer
 
     print('This is a remote shell. Please finish configuration before starting.')
     arg = input(f'The path of kernel image (default - "{kernel_path}"): ')
     kernel_path = arg if len(arg) else kernel_path
-    assert os.path.exists(kernel_path), 'kernel image does not exist.'
+    # assert os.path.exists(kernel_path), 'kernel image does not exist.'
 
     arg = input(f'Port of usb device (default - {port}): ')
     port = arg if len(arg) else port
     communicator.port = port
+    print('')
+
+
+def receive() -> list[str] | None:
+    if not communicator.in_waiting:
+        return None
     
+    responses_bytes = b''
+    while communicator.in_waiting:
+        responses_bytes += communicator.read(communicator.in_waiting)
+        time.sleep(5e-2)
+
+    responses = responses_bytes.decode().split(sep='\n')
+
+    return responses
+
 
 def shell():
+    global communicator
+
     try:
         communicator.open()
         while True:
-            if communicator.in_waiting:
-                termios.tcflush(sys.stdin, termios.TCIFLUSH)
-                response = communicator.read_until().decode(encoding='ascii')
-                if re.match(r'^\$[^$]+\$\n'):
-                    command = response[1: -2]
-                    execute(command)
-                else:
-                    print(f'\r{response}'.ljust(line_width))
-                    
+            responses = receive()
+            if responses:
+                for response in responses:
+                    if response == '# ':
+                        termios.tcflush(sys.stdin, termios.TCIFLUSH)
+                        print(f'\r{response}', end='')
+                    else:
+                        response = response[:-1]
+                        if re.match(r'^\$[^$]+\$', response):
+                            execute(response[1:-1])
+                        elif response != request[:-1]:
+                            print(response)
             else:
-                rlist, _, _ = select.select([sys.stdin], [], [], 1e-2)
+                rlist, _, _ = select.select([sys.stdin], [], [], 5e-2)
                 if rlist:
-                    request = input()
+                    request = f'{input()}\n'
                     if 'exit' in request:
-                        print('\rbye'.ljust(line_width))
+                        print('\rbye')
                         sys.exit()
 
-                    communicator.write(request.encode(encoding='ascii'))
-
+                    communicator.write(request.encode())
     except serial.SerialException as e:
         print(e)
     except KeyboardInterrupt as e:
         print('\nBye!')
     finally:
-        communicator.close()
-    
+        if communicator.is_open:
+            communicator.close() 
 
 def execute(command):
-    print(f'\rexecute interactive command "{command}" ...'.ljust(line_width))
+    print(f'\rexecute interactive command "{command}" ...')
 
     if command == 'upload_kernel':
         upload_kernel()
 
 
 def upload_kernel():
+    global communicator
+
     kernel_size = os.stat(kernel_path).st_size
-    print(f'\rsending kernel size {kernel_size} ...'.ljust(line_width))
-    communicator.write(f'{kernel_size}\n'.encode(encoding='ascii'))
+    print(f'\rsending kernel size {kernel_size} ...')
+    communicator.write(f'{kernel_size}\n'.encode())
     response = communicator.read_until()
     assert '$start_upload\n' == response, 'upload_kernel fail - wrong response 1'
-    print(f'\rsending ...'.ljust(line_width))
+    print(f'\rsending ...')
     with open(kernel_path, 'rb') as f:
         communicator.write(f.read())
     
     response = communicator.read_until()
     assert '$done\n' == response, 'upload_kernel fail - wrong response 2'
-    print(f'\rdone'.ljust(line_width))
+    print(f'\rdone')
     
 
 if __name__ == '__main__':
     assert os.name == 'posix', 'Only support Unix-like operating system.'
     configure_shell()
-    communicator = serial.Serial(port=port, baudrate=baudrate)
     shell()
 
     
