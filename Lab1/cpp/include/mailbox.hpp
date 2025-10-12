0d2939e890891d88a0fff146d6bb5a960e3014a5
Rein Wu <gama79530@gmail.com>
2025-10-12 14:05:39 +0800

    Remove commented-out code

--- Lab1/cpp/include/mailbox.hpp
+++ Lab1/cpp/include/mailbox.hpp
@@ -6,14 +6,6 @@
 #define CHANNEL_MASK 0xF
 #define TIMEOUT_MAX 1000000
 
-// #define MAILBOX_SUCCESS 0
-// #define MAILBOX_ERR_RESPONSE -1
-// #define MAILBOX_ERR_TIMEOUT -2
-
-// volatile uint32_t *get_default_buffer(void);
-// int32_t mailbox_exchange(uint8_t channel, volatile uint32_t *buffer);
-// const char* err_code_to_str(int32_t err_code);
-
 class Mailbox
 {
 public:
